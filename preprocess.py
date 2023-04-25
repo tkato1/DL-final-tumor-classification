6f713be4afc7394f954c605cddebd1558a36d2a9
Alex Zheng <alexander_zheng@brown.edu>
2023-04-25 13:24:03 -0400

    downsampling

--- preprocess.py
+++ preprocess.py
@@ -5,31 +5,40 @@ import mat73
 import numpy as np
 import matplotlib.pyplot as plt
 
-# specify the input directory containing .mat files
-input_dir = "raw_data/test"
+from skimage.measure import block_reduce
 
-def load_data(input_dir, process="uncropped"):
+
+# specify the input directory containing .mat files
+input_dir = "data/test"
+
+def load_data(input_dir, process="uncrop", downsampling_factor=1):
     """
     input_dir: directory of raw .mat files
-    process: the modification to the image(uncropped, cropped, segmented)
+    process: the modification to the image(uncrop, crop, segment)
     """
     raw_images = os.listdir(input_dir)
     number_images = len(raw_images)
 
-    #preinitialization
+    #pre-initialization
     X = np.empty(number_images, dtype=object)
     y = np.empty(number_images, dtype='uint8')
 
     for i, filename in enumerate(raw_images):
-        if filename.endswith('.mat'):
-            # load the .mat file using scipy.io.loadmat()
-            data = mat73.loadmat(os.path.join(input_dir, filename))['cjdata']
-            # print(data.keys()) ##the keys of the data
+        if filename.endswith('.mat'): # load the .mat file using scipy.io.loadmat()
+            data = mat73.loadmat(os.path.join(input_dir, filename))['cjdata'] #dict_keys(['PID', 'image', 'label', 'tumorBorder', 'tumorMask'])
             image_data = np.asarray(data['image'].astype('uint8'))
-            image_data = image_data.reshape((-1, 1))
-            X[i] = image_data
+            downsampled_image = downsample(image_data, factor=downsampling_factor) #downsampling image
+            X[i] = downsampled_image
             y[i] = data['label']
+
     return X, y
+
+def downsample(image, factor=1):
+    """
+    image: a 2d array where each entry represents pixel of an image
+    factor: a integer factor by which to shrink the image by
+    """
+    return block_reduce(image, block_size=(factor, factor), func=np.mean)
 
 def visualize(image):
     """
@@ -39,7 +48,8 @@ def visualize(image):
     plt.show()
 
 if __name__ == "__main__":
-    X, y = load_data(input_dir)
-    print(f"X shape: {X.shape}, Y shape: {y.shape}")
+    X, y = load_data(input_dir, process='uncrop', downsampling_factor=1)
+    print(f"X shape: {X[0].shape}, Y shape: {y.shape}")
+    visualize(X[0])
     print(f"X: {X}") #[[image1], [image2], [image3]] where images are 2d arrays with entries representing pixels of the image
     print(f"Y: {y}") #[label1, label2, label3] where labels in {1., 2., 3.} representing class of tumor
